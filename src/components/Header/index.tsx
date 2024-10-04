b9a2b03b90fd528c760e021df92e511a01fe907e
GiovaniFZ <giovanifinazzi@gmail.com>
2024-10-04 13:51:33 -0300

    feat: Implement modal (to add new transaction)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,29 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
-import logoImg from '../../assets/ignite.svg'
+import logoImg from '../../assets/ignite.svg';
+import * as Dialog from "@radix-ui/react-dialog";
 
-export function Header(){
+// asChild: Aproveita o mesmo botão existente, sem criar um outro.
+
+export function Header() {
     return (
         <HeaderContainer>
             <HeaderContent>
                 <img src={logoImg} alt="" />
-                <NewTransactionButton>Nova transação</NewTransactionButton>
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTransactionButton>Nova transação</NewTransactionButton>
+                    </Dialog.Trigger>
+
+                    <Dialog.Portal>
+                        <Dialog.Overlay>
+                            <Dialog.Content>
+                                <Dialog.Title>Nova transação</Dialog.Title>
+                                <Dialog.Close />
+                            </Dialog.Content>
+                        </Dialog.Overlay>
+                    </Dialog.Portal>
+
+                </Dialog.Root>
             </HeaderContent>
         </HeaderContainer>
     )
